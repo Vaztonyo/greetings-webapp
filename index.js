ab8522ac010cc1782c14a35b8fb9167cb4e3e466
Vaztonyo <tony.dewet7@gmail.com>
2017-08-17 16:07:18 +0200

    Got The counter to work

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 'use strict';
-//***************Require All Needed Modules***************
+//Require All Needed Modules
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var flash = require('express-flash');
 
 var app = express();
 
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: true
+    }
 }))
+app.use(flash());
 
 app.engine('handlebars', exphbs({ // set the app engine to handlebars
     defaultLayout: 'main' // set the default layout to main
@@ -29,86 +33,78 @@ app.use(express.static('public')); //use static and set it to public
 app.use(express.static('views')); //use static views
 
 
-var greetedNames = [];
-var count = 0;
-
 app.get('/greetings', function(req, res, next) {
-  res.render('greetings');
+    res.render('greetings');
 })
 
 app.get('/greeted', function(req, res, next) {
     res.render('greeted');
-  });
+});
 
 app.get('/counter', function(req, res, next) {
     res.render('counter');
-  });
+});
 
-app.post('/greetings', function(req, res, next){
-  var id = req.body.id;
-  var lang = req.body.language;
-  var counter = req.body.counter;
+var greetedNames = [];
+var count = 0;
 
-  var message = "";
+//create a post for the greetings page
+app.post('/greetings', function(req, res, next) {
+    var enteredName = req.body.id;
+    var lang = req.body.language;
+    var counter = req.body.counter;
 
-  if(lang === "english"){
-    message = "Hello, " + id;
-  }
-  else if(lang === "afrikaans"){
-    message = "Goeie dag, " + id;
-  }
-  else{
-    message = "Molo, " + id;
-  }
-  if (id !== "" && lang) {
-    // greetedNames.push(id);
-    var  greetMessage = message;
-  }
+    var message = "";
+    var nameExist = false;
 
-  var nameExist = false;
-  for(id in greetedNames){
-    var nameFound = greetedNames[id];
-    if(nameFound === id){
-      nameExist = true;
-      break;
-    }
-  }
-    if (!nameExist) {
-      greetedNames.push(id);
-    }
-    if (nameExist === false) {
-      count++;
-    }
-    else if(nameExist === true){
-      ;
+    if (lang === "english") {
+        message = "Hello, " + enteredName;
+    } else if (lang === "afrikaans") {
+        message = "Goeie dag, " + enteredName;
+    } else {
+        message = "Molo, " + enteredName;
     }
 
-console.log(count);
+    var foundName = false;
+    console.log(greetedNames);
+    for (var id in greetedNames) {
+        var currentName = greetedNames[id];
+        console.log(currentName);
+        // console.log(enteredName);
+        var namesMatched = currentName.trim() === enteredName.trim();
+        console.log(namesMatched);
 
-var data = {
-  output: greetMessage,
-  counter: count
-}
-  res.render('greetings', data);
+        if (namesMatched) {
+            foundName = true;
+            break;
+        }
+    }
+    console.log(foundName);
+    if (!foundName) {
+        greetedNames.push(enteredName);
+    }
+
+    console.log(count);
+
+    var data = {
+        output: message,
+        counter: greetedNames.length
+    }
+    res.render('greetings', data);
 });
 
 app.post('/greeted', function(req, res, next) {
-  var greetedN = req.body.greetedN;
+    var greetedN = req.body.greetedN;
 
     var namesGreeted = [];
-    for (id in greetedNames) {
-      // if (greetedNames[req.params.id]) {
-      //     greetedNames[req.params.id]++;
-      // } else {
-      //     greetedNames[req.params.id] = 1;
-      // }
-        namesGreeted.push(id);
+    for (id in greetedNames){
+      namesGreeted.push(greetedNames[id]);
     }
     console.log(namesGreeted);
-var data1 = {
-  greetedN : namesGreeted
-}
-    res.render('greeted',data1);
+    var data1 = {
+        greetedN: namesGreeted
+    }
+    res.render('greeted', data1);
 });
 
 app.post('/counter', function(req, res, next) {
